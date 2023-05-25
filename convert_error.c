d52880d2f07c48df47af2b561cf3379a56aced88
Nabil <mkhamisnabil@gmail.com>
2023-05-24 19:40:15 -0700

    Made some changes

--- convert_error.c
+++ convert_error.c
@@ -4,7 +4,7 @@
 *_erratoi - converts a string to an integer
 *@s: the string to be converted
 *
-*Return: 0 if no numbers in string, converted number otherwise 
+*Return: 0 if no numbers in string, converted number otherwise
 *	or -1 on error
 */
 
@@ -12,7 +12,7 @@ int _erratoi(char *s)
 {
 	int i = 0;
 	unsigned long int result = 0;
-	
+
 	if (*s == '+')
 		s++;  /* TODO: why does this make main return 255? */
 
@@ -26,9 +26,9 @@ int _erratoi(char *s)
 				return (-1);
 		}
 		else
-			return (-1);  
+			return (-1);
 	}
-	
+
 	return (result);
 }
 
@@ -52,34 +52,32 @@ void print_error(info_t *info, char *estr)
 }
 
 /**
-*
-*print_d - prints a decimal (integer) number (base 10).
-*
-*@input: the input number.
-*@fd: the file descriptor to write to.
-*
-*Return: The number of characters printed.
-*/
+ * print_d - function prints a decimal (integer) number (base 10)
+ * @input: the input
+ * @fd: the filedescriptor to write to
+ *
+ * Return: number of characters printed
+ */
 int print_d(int input, int fd)
 {
 	int (*__putchar)(char) = _putchar;
 	int i, count = 0;
-	unsigned int abs, current;
+	unsigned int _abs_, current;
 
 	if (fd == STDERR_FILENO)
-		*__putchar = _eputchar; 
+		__putchar = _eputchar;
 	if (input < 0)
 	{
-		abs = -input;
+		_abs_ = -input;
 		__putchar('-');
 		count++;
 	}
 	else
-		abs = input;
-	current = abs;
+		_abs_ = input;
+	current = _abs_;
 	for (i = 1000000000; i > 1; i /= 10)
 	{
-		if (abs / i)
+		if (_abs_ / i)
 		{
 			__putchar('0' + current / i);
 			count++;
