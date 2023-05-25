cb73e486da0180457429efe08ec04fb931365777
Blessing Fortune <74838811+princessble@users.noreply.github.com>
2023-05-25 07:53:58 +0100

    Update shell.h

--- shell.h
+++ shell.h
@@ -166,6 +166,8 @@ int _stem(char *);
 
 /* toem_converterror.c */
 int _erratoi(char *);
+void print_error(info_t *, char *);
+int print_d(int, int);
 char *convert_number(long int, int, int);
 void remove_comments(char *);
 
