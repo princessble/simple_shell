ce03cac4c2cf0955a5be6718438727752f5474d2
Blessing Fortune <74838811+princessble@users.noreply.github.com>
2023-05-25 09:13:31 +0100

    Update convert_error.c

--- convert_error.c
+++ convert_error.c
@@ -20,11 +20,11 @@ result *= 10;
 result += (s[i] - '0');
 if (result > INT_MAX)
 return (-1);
-        }
-        else
-            return (-1);
-    }
-    return (result);
+}
+else
+return (-1);
+}
+return (result);
 }
 
 /**
@@ -36,11 +36,11 @@ void print_error(info_t *info, char *estr)
 {
     _eputs(info->fname);
     _eputs(": ");
-    print_d(info->line_count, STDERR_FILENO);
-    _eputs(": ");
-    _eputs(info->argv[0]);
-    _eputs(": ");
-    _eputs(estr);
+print_d(info->line_count, STDERR_FILENO);
+_eputs(": ");
+_eputs(info->argv[0]);
+_eputs(": ");
+_eputs(estr);
 }
 
 /**
@@ -55,32 +55,30 @@ int print_d(int input, int fd)
     int (*__putchar)(char) = _putchar;
     int i, count = 0;
     unsigned int _abs_, current;
-
-    if (fd == STDERR_FILENO)
-        __putchar = _eputchar;
-
+ if (fd == STDERR_FILENO)
+ __putchar = _eputchar;
     if (input < 0)
-    {
-        _abs_ = -input;
-        __putchar('-');
+{
+ _abs_ = -input;
+__putchar('-');
         count++;
-    }
-    else
-        _abs_ = input;
-    current = _abs_;
-    for (i = 1000000000; i > 1; i /= 10)
-    {
-        if (_abs_ / i)
-        {
-            __putchar('0' + current / i);
-            count++;
-        }
-        current %= i;
-    }
-    __putchar('0' + current);
-    count++;
+}
+else
+_abs_ = input;
+current = _abs_;
+for (i = 1000000000; i > 1; i /= 10)
+{
+if (_abs_ / i)
+{
+__putchar('0' + current / i);
+count++;
+}
+current %= i;
+}
+__putchar('0' + current);
+count++;
 
-    return (count);
+return (count);
 }
 
 /**
