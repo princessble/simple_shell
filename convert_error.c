a04a80af5633aba2d194e7b4fe6364264188d517
princessble <kwomofortuneb@gmail.com>
2023-05-25 01:04:24 -0700

    update

--- convert_error.c
+++ convert_error.c
@@ -1,4 +1,5 @@
 #include "shell.h"
+
 /**
  * _erratoi - converts a string to an integer
  * @s: the string to be converted
@@ -7,29 +8,25 @@
  */
 int _erratoi(char *s)
 {
-	int i = 0;
-	unsigned long int result = 0;
+    int i = 0;
+    unsigned long int result = 0;
 
-	if (*s == '+')
-		s++;
+    if (*s == '+')
+        s++;
 
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			result *= 10;
-
-			result += (s[i] - '0');
-
-				result += (s[i] - '0');
-
-			if (result > INT_MAX)
-				return (-1);
-		}
-		else
-			return (-1);
-	}
-	return (result);
+    for (i = 0; s[i] != '\0'; i++)
+    {
+        if (s[i] >= '0' && s[i] <= '9')
+        {
+            result *= 10;
+            result += (s[i] - '0');
+            if (result > INT_MAX)
+                return (-1);
+        }
+        else
+            return (-1);
+    }
+    return (result);
 }
 
 /**
@@ -39,13 +36,13 @@ int _erratoi(char *s)
  */
 void print_error(info_t *info, char *estr)
 {
-	_eputs(info->fname);
-	_eputs(": ");
-	print_d(info->line_count, STDERR_FILENO);
-	_eputs(": ");
-	_eputs(info->argv[0]);
-	_eputs(": ");
-	_eputs(estr);
+    _eputs(info->fname);
+    _eputs(": ");
+    print_d(info->line_count, STDERR_FILENO);
+    _eputs(": ");
+    _eputs(info->argv[0]);
+    _eputs(": ");
+    _eputs(estr);
 }
 
 /**
@@ -57,37 +54,35 @@ void print_error(info_t *info, char *estr)
  */
 int print_d(int input, int fd)
 {
-	int (*__putchar)(char) = _putchar;
-	int i, count = 0;
-	unsigned int _abs_, current;
+    int (*__putchar)(char) = _putchar;
+    int i, count = 0;
+    unsigned int _abs_, current;
 
-	if (fd == STDERR_FILENO)
-		__putchar = _eputchar;
+    if (fd == STDERR_FILENO)
+        __putchar = _eputchar;
 
-		__putchar = _eputchar;
+    if (input < 0)
+    {
+        _abs_ = -input;
+        __putchar('-');
+        count++;
+    }
+    else
+        _abs_ = input;
+    current = _abs_;
+    for (i = 1000000000; i > 1; i /= 10)
+    {
+        if (_abs_ / i)
+        {
+            __putchar('0' + current / i);
+            count++;
+        }
+        current %= i;
+    }
+    __putchar('0' + current);
+    count++;
 
-	if (input < 0)
-{
-	_abs_ = -input;
-	__putchar('-');
-count++;
-}
-	else
-		_abs_ = input;
-	current = _abs_;
-	for (i = 1000000000; i > 1; i /= 10)
-	{
-		if (_abs_ / i)
-		{
-			__putchar('0' + current / i);
-			count++;
-		}
-		current %= i;
-	}
-	__putchar('0' + current);
-	count++;
-
-	return (count);
+    return (count);
 }
 
 /**
@@ -100,40 +95,37 @@ count++;
  */
 char *convert_number(long int num, int base, int flags)
 {
-	static char *array;
-	static char buffer[50];
-	char sign = 0;
-	char *ptr;
-	unsigned long n = num;
+    static char *array;
+    static char buffer[50];
+    char sign = 0;
+    char *ptr;
+    unsigned long n = num;
 
-	if (!(flags & CONVERT_UNSIGNED) && num < 0)
-	{
-		n = -num;
-		sign = '-';
-	}
+    if (!(flags & CONVERT_UNSIGNED) && num < 0)
+    {
+        n = -num;
+        sign = '-';
+    }
 
-	array = flags & CONVERT_LOWERCASE ? "0123456789abcdef" : "0123456789ABCDEF";
-	ptr = &buffer[49];
+    array = flags & CONVERT_LOWERCASE ? "0123456789abcdef" : "0123456789ABCDEF";
+    ptr = &buffer[49];
+    *ptr = '\0';
 
-	array = flags & CONVERT_LOWERCASE ? "0123456789abcdef" : "012345";
-		ptr = &buffer[49];
+    do {
+        *--ptr = array[n % base];
+        n /= base;
+    } while (n != 0);
 
-	*ptr = '\0';
-
-	do {
-		*--ptr = array[n % base];
-		n /= base;
-	} while (n != 0);
-
-	if (sign)
-		*--ptr = sign;
-	return (ptr);
+    if (sign)
+        *--ptr = sign;
+    return (ptr);
 }
 
 /**
  * remove_comments - function replaces first instance of '#' with '\0'
  * @buf: address of the string to modify
  */
+
 void remove_comments(char *buf)
 {
 	int i;
@@ -147,4 +139,3 @@ void remove_comments(char *buf)
 		}
 	}
 }
-
